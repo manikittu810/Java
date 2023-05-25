a605f344c8c15c46773e3145e6c8ede86a2386ef
MANIKRISHNA SANGANABATLA <58840319+manikittu810@users.noreply.github.com>
2023-05-24 17:21:26 -0700

    email validator in regex

---
+++ EmailValidatorStrict.java
@@ -0,0 +1,23 @@
+
+package com.mkyong.regex.email;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidatorStrict {
+
+    private static final String EMAIL_PATTERN =
+            "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+
+    public static boolean isValid(final String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+}
+
+
+for more examples : https://mkyong.com/regular-expressions/how-to-validate-email-address-with-regular-expression/
